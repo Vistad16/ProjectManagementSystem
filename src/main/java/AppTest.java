bcaf62b3314c0108ffb2be38bc3e18d3d02f9987
Bohdan Datsiv <bohdan.datsiv@gmail.com>
2022-05-26 10:55:54 +0300

    update to gradle

--- src/main/java/AppTest.java
+++ src/main/java/AppTest.java
@@ -1,5 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        String connectionUrl = "jdbc:h2:./testdb";
+        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+            Statement statement = connection.createStatement();
+            //statement.executeUpdate("CREATE TABLE test_table (name VARCHAR(100))");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
